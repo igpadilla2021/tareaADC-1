6e0575fe36ac95b2cd610bc24453845a45720d5b
mauromg18 <136005154+mauromg18@users.noreply.github.com>
2023-10-05 20:12:43 -0300

    getByte.c

--- getByte.c
+++ getByte.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int getByte(int x,int n) {
+    int byte = (x>>(n*8))&0xFF;
+    return byte;
+}
+
+int main() {
+    int hex,n;
+    scanf("%x%d",&hex,&n);
+    int res = getByte(hex,n);
+    printf(" 0x%02X\n",res);
+    return 0;
+}
 
